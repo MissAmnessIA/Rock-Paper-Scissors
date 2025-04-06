15895fc9b81cccc265e9627efe0330cb76a26aea
MissAmnessIA <veronicajoanna30@gmail.com>
2025-04-06 17:39:18 +0200

    Finished

--- javas.js
+++ javas.js
@@ -1,48 +1,89 @@
 
+let computerChoice;
+let humanChoice;
+
 function getComputerChoice(max)
 {
     let random = Math.floor(Math.random() * max);
     if (random == 0)
-        computerChoice ="Rock";
+        computerChoice ="rock";
     else if (random == 1)
-        computerChoice ="Peper";
+        computerChoice ="paper";
     else
-        computerChoice = "Scisors";
+        computerChoice = "scisors";
 }
 
 function getHumanChoice()
 {
-    return prompt("Your choose:")
+    humanChoice = prompt("Pick one:")
+    humanChoice = humanChoice.toLowerCase();
 }
 
 function playRound(computerChoice, humanChoice)
 {
+    let roundScore;
 
+    if (computerChoice == humanChoice)
+        console.log("No one wins");
+    if (computerChoice == "rock")
+    {
+        if (humanChoice == "paper")
+            roundScore = 1;
+        else if (humanChoice == "scisors")
+            roundScore = -1;
+    }
+    else if (computerChoice == "paper")
+    {
+        if (humanChoice == "rock")
+            roundScore = -1;
+        else if (humanChoice == "scisors")
+            roundScore = 1;
+    }
+    else if (computerChoice == "scisors")
+    {
+        if (humanChoice == "rock")
+            roundScore = 1;
+        else if (humanChoice == "paper")
+            roundScore = -1;
+    }
+    return roundScore;
 }
 
 function playGame()
 {
-    let computerChoice;
-    let humanChoice;
-    let humanScore;
-    let computerScore;
-    let score;
+    let computerScore = 0;
+    let humanScore = 0;
+    let roundScore;
 
     for (let round = 1; round <= 5; round++)
     {
-        computerChoice = getComputerChoice(3);
-        humanChoice = getHumanChoice();
-        score = playRound(computerChoice, humanChoice);
-        if (score === 1)
+        getComputerChoice(3);
+        getHumanChoice();
+        console.log(`Your choice: ${humanChoice}`);
+        console.log(`Computer choice: ${computerChoice}`);
+        roundScore = playRound(computerChoice, humanChoice);
+        if (roundScore == 1)
+        {
+            console.log(`YOU WIN!!`);
             humanScore++;
-        if (score === 0)
+        }
+        else if (roundScore == -1)
+        {
+            console.log(`YOU LOSE...`);
             computerScore++;
-        console.log(`Round ${round}: Human ${humanScore} | Computer ${computerScore}`);
+        }
+        console.log(`${humanScore} | ${computerScore}`);
+        console.log(" ");
     }
     if (computerScore > humanScore)
         console.log("The machine wins again...");
     else if (computerScore < humanScore)
         console.log("Congratulations...");
+    else if (computerScore == humanScore)
+        console.log(`No one wins     ${humanScore} | ${computerScore}`);
 }
+
+playGame();
+
 
 
